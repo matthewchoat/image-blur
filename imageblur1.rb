dfaa37074e4607b3256469192c8b1b32b9c5b8e1
matthewchoat <matthewchoat@gmail.com>
2019-07-07 23:28:47 +0000

    imageblur pt.1

---
+++ imageblur1.rb
@@ -0,0 +1,20 @@
+class Image
+
+  def initialize(image)
+    @image = image
+  end
+
+  def output_image
+    @image.each do |cell|
+      puts cell.join
+    end
+  end
+end
+
+image = Image.new([
+  [0, 0, 0, 0],
+  [0, 1, 0, 0],
+  [0, 0, 0, 1],
+  [0, 0, 0, 0]
+])
+image.output_image
